68b519014c470ac84f284f195984c8e9c6c9f78b
Francisca Moreno <mfrancisca.moreno@gmail.com>
2026-03-12 15:36:57 -0500

    feat(06-01): add robots.txt route and template (staging: Disallow all)
    - Add TemplateView robots.txt route at /robots.txt in config/urls.py
    - Route placed before Wagtail catch-all, serves as text/plain
    - Create core/templates/robots.txt with Disallow: / (staging mode)
    - Template will be updated to Allow all at DNS cutover in Plan 04

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,16 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.views.generic import TemplateView
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
+    path('robots.txt', TemplateView.as_view(
+        template_name='robots.txt',
+        content_type='text/plain',
+    )),
     path('pigments/', include('pigments.urls')),
     path('cms/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
